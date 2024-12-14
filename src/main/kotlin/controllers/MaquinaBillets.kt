dafaa6a39a632f6d827cc2d497cbdf329fabeb8b
Cuenquis777 <marc.cuenca.7e8@itb.cat>
2024-12-14 19:52:06 +0100

    Commit

---
+++ src/main/kotlin/controllers/MaquinaBillets.kt
@@ -0,0 +1,24 @@
+package org.example.controllers
+
+fun main() {
+    menu()
+
+
+}
+
+
+
+
+//Hacemos la funcion del menu
+fun menu (){
+    println("1. Bitllet senzill\n" +
+            "2. TCasual\n" +
+            "3. TUsual\n" +
+            "4. TFamiliar\n" +
+            "5. TJove")
+}
+
+//Debemos creear 5 funciones 1 para cada 1 que haga su funcion
+fun menuFuncional ()
+//Que se pueda tirar para atras
+//Hacerr un menu funcional, que si el usuario escoga el 1 vaya a la funcion x
